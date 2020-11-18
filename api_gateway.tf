2195ba5a9137bba954fb6edc412d76efa89d3d49
javier <javi_mr@me.com>
2020-11-18 16:33:04 +0000

    Added extra options.

--- api_gateway.tf
+++ api_gateway.tf
@@ -21,15 +21,15 @@ resource "aws_api_gateway_rest_api" "api_1" {
 
 //Create resource
 resource "aws_api_gateway_resource" "send_to_queue_resource" {
-    rest_api_id = aws_api_gateway_rest_api.api_1.id 
+    rest_api_id = aws_api_gateway_rest_api.api_1.id
     parent_id = aws_api_gateway_rest_api.api_1.root_resource_id
-    path_part = "{proxy+}"
+    path_part = "sqs"
 }
 
 //Create method for the resource
 resource "aws_api_gateway_method" "send_to_queue_method" {
     rest_api_id = aws_api_gateway_resource.send_to_queue_resource.rest_api_id
-    resource_id = aws_api_gateway_resource.send_to_queue_resource.id 
+    resource_id = aws_api_gateway_resource.send_to_queue_resource.id
     http_method = "ANY"
     authorization = "NONE"
 }
@@ -44,7 +44,15 @@ resource "aws_api_gateway_integration" "send_to_queue" {
     uri = "arn:aws:apigateway:eu-west-1:sqs:path/${aws_sqs_queue.first_sqs_queue.name}"
     credentials = aws_iam_role.api_role.arn
     integration_http_method = "POST"
-    passthrough_behavior = "NEVER"
+    //passthrough_behavior = "WHEN_NO_MATCH"
+
+    //request_parameters = {
+    //  "integration.request.header.Content-Type" = "'application/json'"
+    //}
+
+    //request_templates = {
+    //  "application/json" = "Action=SendMessage&MessageBody=$input.body"
+    //}
 }
 
 //Response resource from SQS
@@ -52,7 +60,16 @@ resource "aws_api_gateway_integration_response" "queue_response" {
     rest_api_id = aws_api_gateway_method.send_to_queue_method.rest_api_id
     resource_id = aws_api_gateway_method.send_to_queue_method.resource_id
     http_method = aws_api_gateway_method.send_to_queue_method.http_method
+    selection_pattern = "^2[0-9][0-9]"
     status_code = 200
+
+    //response_templates = {
+    //  "application/json" = "{\"message\": \"Great success\"}"
+    //}
+
+    depends_on = [
+      aws_api_gateway_integration.send_to_queue
+    ]
 }
 
 //Response method from SQS
@@ -61,6 +78,10 @@ resource "aws_api_gateway_method_response" "api_response" {
     resource_id = aws_api_gateway_integration_response.queue_response.resource_id
     http_method = aws_api_gateway_integration_response.queue_response.http_method
     status_code = aws_api_gateway_integration_response.queue_response.status_code
+
+    //response_models = {
+    //  "application/json" = "Empty"
+    //}
 }
 //******
 
